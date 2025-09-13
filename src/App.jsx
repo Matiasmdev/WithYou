70d6321d4ac93ee29c30a3fdf626edf3dd194b4a
Matiasdev <matiasfrontend@gmail.com>
2025-09-12 22:29:55 -0300

    agregue punto

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const WelcomeScreen = ({ onCreateRoom, onJoinRoom }) => {
       <div className="bg-gray-900/20 py-2 text-center absolute bottom-0 w-full">
         <p className="text-white/70 text-sm flex flex-col">
           <span>© 2025 WithYou.</span> 
-          <span>Desarrollado por <a href="https://github.com/matiascoder" target="_blank" rel="noopener noreferrer" className="text-teal-400 hover:text-teal-300">@matiascoder</a></span>
+          <span>Desarrollado por <a href="https://github.com/matiascoder" target="_blank" rel="noopener noreferrer" className="text-teal-400 hover:text-teal-300">@matiascoder</a>.</span>
         </p>
       </div>
     </div>
